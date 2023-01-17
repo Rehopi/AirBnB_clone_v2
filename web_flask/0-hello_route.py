fa81abba770cfc441a5bebe327fb8503fc18003e
rehopi <emekaokechukwu16@gmail.com>
2023-01-17 13:18:18 -0800

    Hello Flask!
    mandatory

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Hello Flask!
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
